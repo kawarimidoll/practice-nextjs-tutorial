0dae249f56863a79c5a09899e0adb1a9c702f932
kawarimidoll <kawarimidoll+git@gmail.com>
2025-03-03 00:21:19 +0900

    chapter 14 first half

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,26 +9,51 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Please enter an amount greater than $0.'
+  }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  message?: string | null;
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  }
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // Test it out:
+  console.log({ data: Object.fromEntries(formData.entries()), validatedFields });
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      message: 'Missing Fields. Failed to create invoice.',
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  // Prepare data for insertion
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-  // Test it out:
-  console.log({ customerId, amount, status });
-  console.log(Object.fromEntries(formData.entries()));
 
   try {
     await sql`
@@ -36,8 +61,10 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
-    // Wee'll log the error to the console for now
-    console.error(error);
+    // If a database error occurs, return a more specific error message.
+    return {
+      message: 'Database Error. Failed to create invoice.',
+    }
   }
 
   revalidatePath('/dashboard/invoices');
