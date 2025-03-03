a79ffca4c601c44e10784a67756fde3518e6229d
kawarimidoll <kawarimidoll+git@gmail.com>
2025-03-03 12:34:32 +0900

    chapter 15

---
+++ auth.config.ts
@@ -0,0 +1,24 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) {
+        // Keep staying authenticated users on dashboard page
+        if (isLoggedIn) return true;
+        // Redirect unauthenticated users to login page
+        return false;
+      } else if (isLoggedIn) {
+        // Redirect authenticated users to dashboard page
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
