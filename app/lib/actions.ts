860ae8d34b01ecc9b731e574c3f4bd208f8680b7
kawarimidoll <kawarimidoll+git@gmail.com>
2025-03-03 10:41:01 +0900

    chapter 14 second half

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -73,12 +73,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // Test it out:
+  console.log({ data: Object.fromEntries(formData.entries()), validatedFields });
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      message: 'Missing Fields. Failed to create invoice.',
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
@@ -89,8 +102,10 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
   } catch (error) {
-    // Wee'll log the error to the console for now
-    console.error(error);
+    // If a database error occurs, return a more specific error message.
+    return {
+      message: 'Database Error. Failed to create invoice.',
+    }
   }
 
   revalidatePath('/dashboard/invoices');
